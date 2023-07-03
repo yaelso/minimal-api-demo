79de2901ad74784b271e0eb0152435aa7f733a4a
Yael <yaelasolomon@gmail.com>
2023-07-02 21:24:28 -0700

    Extend Pizza model properties by adding description

--- PizzaStore/PizzaStore/Db.cs
+++ PizzaStore/PizzaStore/Db.cs
@@ -4,15 +4,16 @@ public record Pizza
 {
     public int Id { get; set; }
     public string ? Name { get; set; }
+    public string ? Description { get; set; }
 }
 
 public class PizzaDB
 {
     private static List<Pizza> _pizzas = new List<Pizza>()
     {
-        new Pizza { Id = 1, Name = "Cheesy Greens, featuring a creamy vegan cheese blend layered over a bed of sauteed spinach and kale" },
-        new Pizza { Id = 2, Name = "Smoky BBQ Jackfruit, a tantalizing combination of tender pulled jackfruit infused with a tangy barbecue sauce and caramelized onions" },
-        new Pizza { Id = 3, Name = "Mediterranean Delight, adorned with marinated artichoke hearts, sun-dried tomatoes, Kalamata olives, roasted garlic, and fragrant oregano" }
+        new Pizza { Id = 1, Name = "Cheesy Greens", Description = "A creamy vegan cheese blend layered over a bed of sauteed spinach and kale"},
+        new Pizza { Id = 2, Name = "Smoky BBQ Jackfruit", Description = "A tantalizing combination of tender pulled jackfruit infused with a tangy barbecue sauce and caramelized onions" },
+        new Pizza { Id = 3, Name = "Mediterranean Delight", Description = "Adorned with marinated artichoke hearts, sun-dried tomatoes, Kalamata olives, roasted garlic, and fragrant oregano" }
     };
 
     public static List<Pizza> GetPizzas()
